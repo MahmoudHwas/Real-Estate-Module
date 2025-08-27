24a86e59bc929525f2c22ceccdaba3ba4e2ed964
MahmoudHwas <mahmoudhwas62@gmail.com>
2025-08-27 10:11:44 +0300

    d

--- app_one/__manifest__.py
+++ app_one/__manifest__.py
@@ -3,22 +3,38 @@
     "author": "Mahmoud Hawas",
     "category": "Real Estate",
     "version": "17.0.0.1.0",
-    "depends": ["base", "sale_management", "account", "mail", "contacts"],
+    "depends": [
+        "base",
+        "web",
+        "sale_management",
+        "account",
+        "mail",
+        "contacts"
+    ],
     "data": [
         "security/ir.model.access.csv",
+        "data/data.xml",
         "views/base_menu.xml",
         "views/property_view.xml",
         "views/owner_view.xml",
         "views/tag_view.xml",
         "views/sale_order_view.xml",
         "views/res_partner_view.xml",
+        "views/history_view.xml",
         "views/building_view.xml",
+        "wizard/change_state_wizard.xml",
         "reports/property_report.xml",
-
-
     ],
-    'assets': {
-        'web.assets_backend' : ['app_one\static\src\css\property.css']
+    "assets": {
+        "web.assets_backend": [
+            "app_one/static/src/css/property.css",
+            "app_one/static/src/components/listView/listView.js",
+            "app_one/static/src/components/listView/listView.xml",
+            "app_one/static/src/components/listView/listView.css",
+            "app_one/static/src/components/formView/formVeiw.js",
+            "app_one/static/src/components/formView/formView.xml",
+            "app_one/static/src/components/formView/formVeiw.css",
+        ],
     },
     "application": True,
 }
